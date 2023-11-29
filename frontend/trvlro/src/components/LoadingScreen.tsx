5980c001600e62d4d4054893e37cb909d8102053
stefanfaur <stefan.faur@student.upt.ro>
2023-11-29 16:40:29 +0200

    added LoadingScreen component

---
+++ frontend/trvlro/src/components/LoadingScreen.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { Spin } from 'antd';
+
+const LoadingScreen: React.FC = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Spin size="large" />
+    </div>
+  );
+};
+
+export default LoadingScreen;
