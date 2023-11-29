86323343ce66746f85abeefc615ee1db5894d43d
stefanfaur <stefan.faur@student.upt.ro>
2023-11-29 18:54:14 +0200

    updated buttons

--- frontend/trvlro/src/pages/HomePage.tsx
+++ frontend/trvlro/src/pages/HomePage.tsx
@@ -2,15 +2,29 @@ import React from 'react';
 import { Button } from 'antd';
 import '../styles/HomePage.css';
 import logo from '../assets/logo_comp_transparent.png';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage: React.FC = () => {
+    
+    const navigate = useNavigate();
+
+    const handleRegisterClick = () => {
+        navigate("/authentication/register"); 
+    };
+
     return (
         <div className="homepage">
             <img src={logo} alt="Logo" className="logo" />
             <h1 className="title">Discover the new way <br></br>of travelling.</h1>
             <p className="subtitle">Start today, with us!</p>
             <div className="buttons">
-                <Button type="primary" className="register-button">Register Now</Button>
+                <Button 
+                    type="primary" 
+                    className="register-button"
+                    onClick={handleRegisterClick} 
+                >
+                    Register Now
+                </Button>
                 <div className="button-separator"></div>
                 <p className="login-text">Already an user? <a href="/authentication/login">Log in</a></p>
             </div>
--- frontend/trvlro/src/pages/RegisterPage.tsx
+++ frontend/trvlro/src/pages/RegisterPage.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
+import '../styles/RegisterPage.css';
+import logo from '../assets/logo_text_transparent.png';
+
+import googleIcon from '../assets/google.svg';
+import facebookIcon from '../assets/facebook.svg';
+import { Button } from 'antd';
+
+
 
 const LoginPage: React.FC = () => {
+
     return (
-        <div className="login-page">
+        <div className="register-page">
             <div className="logo">
-                {/* Your logo component */}
+                <img src={logo} alt="Logo" className="logo" />
             </div>
             <p className="text">Login and start your travels.</p>
             <div className="account-details">
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+                <input type="email" placeholder="Email" className="input"/>
+                <input type="password" placeholder="Password" className="input" />
+                <input type="confirm_password" placeholder="Confirm Password" className="input" />
             </div>
-            <button className="login-button">Login</button>
-            <div className="quick-sign-in">
-                <button className="google-button">
-                    {/* Google icon */}
-                </button>
-                <button className="facebook-button">
-                    {/* Facebook icon */}
-                </button>
+            <Button type="primary" className="register-button">Register</Button>
+            <div className="quick">
+                <p className="bottom_text">Or register with</p>
+                <Button className="facebook-button" value="Face">
+                    <img src={facebookIcon} alt="Facebook Icon" className="facebook-icon" />
+                </Button>
+                <Button className="google-button">
+                    <img src={googleIcon} alt="Google Icon" className="google-icon" />
+                </Button>
             </div>
         </div>
     );
