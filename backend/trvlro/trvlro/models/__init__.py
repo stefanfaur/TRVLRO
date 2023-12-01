6e6f034a97fc1a3db1f3a4403d84a52e6015edb1
stefanfaur <stefan.faur@student.upt.ro>
2023-12-02 00:54:41 +0200

    detailed reasoning

--- backend/trvlro/trvlro/models/__init__.py
+++ backend/trvlro/trvlro/models/__init__.py
@@ -1,1 +1,8 @@
-"""Data models for trvlro."""
+"""
+Data models for trvlro.
+Given that we're using Firestore, we're schema-less
+so we're using Pydantic for validation.
+*especially since we will get some data from an LLM, 
+which may not always be in the proper format*
+We NEED to ensure we only store it if it's in the proper format.
+"""
