2fe60c9ee80351e0dce896d023ce122a95dc16c2
stefanfaur <stefan.faur@student.upt.ro>
2024-01-02 21:15:00 +0200

    update text

--- backend/trvlro/trvlro/models/__init__.py
+++ backend/trvlro/trvlro/models/__init__.py
@@ -2,6 +2,8 @@
 Data models for trvlro.
 Given that we're using Firestore, we're schema-less
 so we're using Pydantic for validation.
+FastAPI now uses PydanticV2 so even with our large and growing data,
+speed isn't an issue, the models will scale well.
 *especially since we will get some data from an LLM, 
 which may not always be in the proper format*
 We NEED to ensure we only store it if it's in the proper format.
